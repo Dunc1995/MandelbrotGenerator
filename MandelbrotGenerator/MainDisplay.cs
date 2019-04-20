41dd2b99a37ad6d68a277171aa8e8ae88060441b
Duncan Bailey <DPBailey1995@outlook.com>
2019-04-20 10:55:32 +0100

    testing scaling limits are correct. Bounds of the Mandelbrot Set are at magnitude 2 within the complex plane, therefore a circle should be seen...

--- MandelbrotGenerator/MainDisplay.cs
+++ MandelbrotGenerator/MainDisplay.cs
@@ -16,7 +16,7 @@ namespace MandelbrotGenerator
         {
             InitializeComponent();
             int width = pictureBox1.Width;
-            int height = pictureBox1.Height;
+            int height = pictureBox1.Height;         
 
             Bitmap image = new Bitmap(width, height);
             Random rand = new Random();
@@ -25,16 +25,52 @@ namespace MandelbrotGenerator
             {
                 for (int y = 0; y < height; y++)
                 {
-                    int a = rand.Next(256);
-                    int r = rand.Next(256);
-                    int g = rand.Next(256);
-                    int b = rand.Next(256);
+                    int a = 255;
+                    int r = 255;
+                    int g = 255;
+                    int b = 255;
+                    
+                    PointF point = GetFloatingPointValue(x, y);
+
+                    double Magnitude = Math.Sqrt(Math.Pow(point.X, 2) + Math.Pow(point.Y, 2));
+                    if (Magnitude >= 2)
+                    {
+                        r = 0;
+                        g = 0;
+                        b = 0;
+                    }
 
                     image.SetPixel(x, y, Color.FromArgb(a, r, g, b));
                 }
             }
 
             pictureBox1.Image = image;
+        }
+
+        /// <summary>
+        /// Translate pixel coordinates to dimensions within the complex plane.
+        /// Magnitudes greater than 2 will not be within the Mandelbrot Set, therefore
+        /// this determines the bounds within the pixel dimensions.
+        /// </summary>
+        /// <param name="pixelX"></param>
+        /// <param name="pixelY"></param>
+        /// <returns></returns>
+        public PointF GetFloatingPointValue(int pixelX, int pixelY)
+        {
+
+            int width = pictureBox1.Width;
+            int height = pictureBox1.Height;
+            int minorViewportDimension = (width > height) ? height : width;
+
+            float scalingValue = minorViewportDimension / 4;
+
+            float centreX = width / 2;
+            float centreY = height / 2;
+
+            float scaledX = (pixelX - centreX) / scalingValue;
+            float scaledY = (pixelY - centreY) / scalingValue;
+
+            return new PointF(scaledX, scaledY);
         }
     }
 }
